aa5f2c2638529a0026173a638f4d8f0079194374
Travis Herrick <quasimonomial@gmail.com>
2025-03-28 14:19:35 -0700

    add caching layer

---
+++ app/services/cache_service.rb
@@ -0,0 +1,43 @@
+# Redis Docs: https://redis.io/learn/develop/ruby
+# Note: For some types of Cache we would want to keep frequently accessed values refreshed, however Weather itself changes and becomes outdated, we acutally want to refresh it periodically
+
+class CacheService
+  class << self
+    EXPIRATION_INTERVAL = 30.minutes.to_i
+
+    def client
+      @client ||= Redis.new()
+    end
+
+    def read(key)
+      redis_data = client.get(namespaced_key(key))
+      redis_data.present? ? deserialize(redis_data) : nil
+    end
+
+    def write(key, value)
+      client.setex(namespaced_key(key), EXPIRATION_INTERVAL, serialize(value))
+    end
+
+    def delete(key)
+      client.del(namespaced_key(key))
+    end
+
+    def exist?(key)
+      client.exists?(namespaced_key(key))
+    end
+
+    private
+
+    def namespaced_key(key)
+      "weather_vain:#{Rails.env}:#{key}"
+    end
+
+    def serialize(value)
+      value.to_json
+    end
+
+    def deserialize(value)
+      JSON.parse(value, { symbolize_names: true })
+    end
+  end
+end
--- app/services/weather_forecast_service.rb
+++ app/services/weather_forecast_service.rb
@@ -1,15 +1,28 @@
 class WeatherForecastService
   def self.get_forecast_by_zip(zip_code)
-    WeatherForecastService.get_forecast_by_lat_long(zip_code.latitude, zip_code.longitude)
+    if CacheService.exist?(self.cache_forecast_zip_key(zip_code.code))
+      CacheService.read(self.cache_forecast_zip_key(zip_code.code))
+    else
+      WeatherForecastService.get_forecast_by_lat_long(zip_code)
+    end
   end
 
-  def self.get_forecast_by_lat_long(latitude, longitude)
+  def self.get_forecast_by_lat_long(zip_code)
+    latitude = zip_code.latitude
+    longitude = zip_code.longitude
     begin
       forecast = AppleWeatherkitAdapter.get_forecast_from_lat_long(latitude, longitude)
     rescue => e
       forecast = NationalWeatherServiceAdapter.get_forecast_from_lat_long(latitude, longitude)
     end
 
+    CacheService.write(self.cache_forecast_zip_key(zip_code.code), forecast)
+
     forecast
+  end
+
+  private
+  def self.cache_forecast_zip_key(zip_code)
+    "forecast:z:#{zip_code}"
   end
 end
